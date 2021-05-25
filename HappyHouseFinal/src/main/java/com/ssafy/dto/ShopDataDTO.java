66b8324ea79360e5ded8a783be6b188c9985cefd
lsh104305 <lsh104305@naver.com>
2021-05-25 10:24:12 +0900

    shop data 수정

--- HappyHouseFinal/src/main/java/com/ssafy/dto/ShopDataDTO.java
+++ HappyHouseFinal/src/main/java/com/ssafy/dto/ShopDataDTO.java
@@ -1,9 +1,30 @@
 package com.ssafy.dto;
 
 public class ShopDataDTO {
+	private String city;
+	private String gugun;
+	private String dong;
 	private String type;
 	private String count;
 	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getGugun() {
+		return gugun;
+	}
+	public void setGugun(String gugun) {
+		this.gugun = gugun;
+	}
+	public String getDong() {
+		return dong;
+	}
+	public void setDong(String dong) {
+		this.dong = dong;
+	}
 	public String getType() {
 		return type;
 	}
@@ -21,9 +42,11 @@ public class ShopDataDTO {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public ShopDataDTO(String type, String count) {
+	public ShopDataDTO(String city, String gugun, String dong, String type, String count) {
 		super();
+		this.city = city;
+		this.gugun = gugun;
+		this.dong = dong;
 		this.type = type;
 		this.count = count;
 	}
